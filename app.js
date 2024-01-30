9a3f276a98de18ea73e16f1cb516189b3ba2958f
Aditya Gupta <adityabajaj161@gmail.com>
2024-01-30 18:55:31 +0530

    third-commit

--- app.js
+++ app.js
@@ -14,13 +14,13 @@ app.use(bodyParser.json());
 app.use(cors());
 app.use(express.static('public'));
 
-const currentDir = __dirname;
-app.use(express.static(path.join(currentDir, 'frontend', 'dist', 'frontend', 'browser')));
+// const currentDir = __dirname;
+// app.use(express.static(path.join(currentDir, 'frontend', 'dist', 'frontend', 'browser')));
 
-// All other requests return the Angular app
-app.get('*', (req, res) => {
-    res.sendFile(path.join(currentDir, 'frontend', 'dist', 'frontend', 'browser', 'index.html'));
-});
+// // All other requests return the Angular app
+// app.get('*', (req, res) => {
+//     res.sendFile(path.join(currentDir, 'frontend', 'dist', 'frontend', 'browser', 'index.html'));
+// });
 
 // MongoDB connection
 mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true });
